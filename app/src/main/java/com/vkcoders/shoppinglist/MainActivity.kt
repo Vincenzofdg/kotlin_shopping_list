01ad78398b01c8306c198b435f1bb845637fc673
Vincenzofdg <vincenzofdg.dev@gmail.com>
2025-04-04 18:02:24 -0300

    feat: initial config fix

--- app/src/main/java/com/vkcoders/shoppinglist/MainActivity.kt
+++ app/src/main/java/com/vkcoders/shoppinglist/MainActivity.kt
@@ -5,8 +5,8 @@ import android.os.Bundle
 import android.os.PersistableBundle
 
 class MainActivity : Activity() {
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
     }
 
 }
